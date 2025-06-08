d467d37f8f300a101e7ec7c074f68e8eb1f39f84
table106 <cytrynek106@gmail.com>
2025-06-08 22:04:25 +0200

    Add project files.

---
+++ OpeningHelper/OHMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace OpeningHelper
+{
+    partial class OHMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OHMain));
+            this.KeyPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CasePrice = new System.Windows.Forms.TextBox();
+            this.stupidLabel = new System.Windows.Forms.Label();
+            this.CaseCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.FinalPrice = new System.Windows.Forms.Label();
+            this.SplashText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // KeyPrice
+            // 
+            this.KeyPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.KeyPrice.Location = new System.Drawing.Point(186, 109);
+            this.KeyPrice.Name = "KeyPrice";
+            this.KeyPrice.Size = new System.Drawing.Size(100, 26);
+            this.KeyPrice.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(107, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(262, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "OpeningHelper";
+            // 
+            // CasePrice
+            // 
+            this.CasePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CasePrice.Location = new System.Drawing.Point(186, 141);
+            this.CasePrice.Name = "CasePrice";
+            this.CasePrice.Size = new System.Drawing.Size(100, 26);
+            this.CasePrice.TabIndex = 3;
+            // 
+            // stupidLabel
+            // 
+            this.stupidLabel.AutoSize = true;
+            this.stupidLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stupidLabel.Location = new System.Drawing.Point(96, 144);
+            this.stupidLabel.Name = "stupidLabel";
+            this.stupidLabel.Size = new System.Drawing.Size(84, 20);
+            this.stupidLabel.TabIndex = 4;
+            this.stupidLabel.Text = "Case price";
+            // 
+            // CaseCount
+            // 
+            this.CaseCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CaseCount.Location = new System.Drawing.Point(186, 173);
+            this.CaseCount.Name = "CaseCount";
+            this.CaseCount.Size = new System.Drawing.Size(100, 26);
+            this.CaseCount.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(90, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Case count";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Calculate.Location = new System.Drawing.Point(155, 222);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(89, 29);
+            this.Calculate.TabIndex = 7;
+            this.Calculate.Text = "Calculate!";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // FinalPrice
+            // 
+            this.FinalPrice.AutoSize = true;
+            this.FinalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FinalPrice.Location = new System.Drawing.Point(93, 304);
+            this.FinalPrice.Name = "FinalPrice";
+            this.FinalPrice.Size = new System.Drawing.Size(0, 24);
+            this.FinalPrice.TabIndex = 8;
+            // 
+            // SplashText
+            // 
+            this.SplashText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SplashText.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.SplashText.Location = new System.Drawing.Point(20, 52);
+            this.SplashText.Name = "SplashText";
+            this.SplashText.Size = new System.Drawing.Size(352, 17);
+            this.SplashText.TabIndex = 9;
+            this.SplashText.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // OHMain
+            // 
+            this.AcceptButton = this.Calculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.SplashText);
+            this.Controls.Add(this.FinalPrice);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CaseCount);
+            this.Controls.Add(this.stupidLabel);
+            this.Controls.Add(this.CasePrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.KeyPrice);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "OHMain";
+            this.Text = "OpeningHelper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox KeyPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox CasePrice;
+        private System.Windows.Forms.Label stupidLabel;
+        private System.Windows.Forms.TextBox CaseCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label FinalPrice;
+        private System.Windows.Forms.Label SplashText;
+    }
+}
+
---
+++ OpeningHelper/OHMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpeningHelper
+{
+    public partial class OHMain : Form
+    {
+        public static readonly Random random = new Random();
+        private static readonly string[] wear = { "battle-scarred", "well worn", "field tested", "minimal wear", "factory new" };
+        public static readonly string[] splash = { 
+            "or you could just use a calculator",
+            "gold gold gold", 
+            "daring today, aren't we?",
+            "99% of gamblers quit before their big win",
+            $"this text is {wear[random.Next(wear.Length)]}!",
+            "what do you mean i won't profit?",
+            "t",
+            "circle square, circle square...",
+            "POW! haha",
+            "dQw4w9WgXcQ",
+            "now that's class",
+            "you call shooting a coin a martial art??",
+            "exquisite software",
+            "honestly quite incredible",
+            "chat is this fr?",
+            "hi",
+            "it helps openly",
+            "the hobby",
+            "zis is a flammenwerfer, it werfs flammens",
+            "vegan!",
+            "one does not simply win",
+            "mirror mirror on the wall, who's the gambler of them all",
+            "you know that buying skins is an option too, right?",
+            "silksong when", // should probably remove this when it arrives
+            "peak",
+            "apples my beloved",
+            "consider this a blessing",
+            "where did you even find this",
+            "scattered across space and time... is the person who asked",
+            "helpful!"
+        };
+
+        public OHMain()
+        {
+            InitializeComponent();
+            GenerateSplashText();
+        }
+
+        public void GenerateSplashText()
+        {
+            SplashText.Text = splash[random.Next(splash.Length)];
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            GenerateSplashText();
+            if (!double.TryParse(KeyPrice.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double keyPrice))
+            {
+                KeyPrice.Focus();
+                MessageBox.Show("Expected decimal point value for key price", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (keyPrice <= 0)
+            {
+                KeyPrice.Focus();
+                MessageBox.Show("Price cannot be less than or 0", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!double.TryParse(CasePrice.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double casePrice))
+            {
+                CasePrice.Focus();
+                MessageBox.Show("Expected decimal point value for case price", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (casePrice <= 0)
+            {
+                CasePrice.Focus();
+                MessageBox.Show("Price cannot be less than or 0", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!int.TryParse(CaseCount.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out int caseCount))
+            {
+                CaseCount.Focus();
+                MessageBox.Show("Expected integer (whole number)", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (caseCount <= 0)
+            {
+                CaseCount.Focus();
+                MessageBox.Show("Count cannot be less than or 0", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            FinalPrice.Text = "Final price: " + (keyPrice * caseCount + casePrice * caseCount).ToString();
+        }
+    }
+}
